4f69ad1fcdb929b08c268e1b7d93428b43c9831e
Vu Le <lebachvu@gmail.com>
2017-12-29 10:53:56 +0700

    Initial commit

---
+++ app/src/main/java/info/devexchanges/fbslidingmenu/fragment/RadioGroupFragment.java
@@ -0,0 +1,39 @@
+package info.devexchanges.fbslidingmenu.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import info.devexchanges.fbslidingmenu.R;
+
+public class RadioGroupFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_radio_group, container, false);
+        RadioGroup radioGroup = (RadioGroup) view.findViewById(R.id.radio);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                // This will get the radiobutton that has changed in its check state
+                RadioButton checkedRadioButton = (RadioButton) radioGroup.findViewById(i);
+                // This puts the value (true/false) into the variable
+                boolean isChecked = checkedRadioButton.isChecked();
+                // If the radiobutton that has changed in check state is now checked...
+                if (isChecked) {
+                    Toast.makeText(getActivity(), "You checked: " + checkedRadioButton.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view;
+    }
+}
